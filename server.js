f35818a651295bdb0afbaa29c028db6eaf2490cc
Tasniem Seiam <tasniemseiam@gmail.com>
2024-10-17 02:33:41 +0300

    new update

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 
 // dotenv.config({ path: "./config/.env" });
 // const DB = process.env.DB.replace("<db_password>", process.env.DB_PASS);
-const DB = process.env.DB ? process.env.DB.replace("<db_password>", process.env.DB_PASS) : null;
+// const DB = process.env.DB ? process.env.DB.replace("<db_password>", process.env.DB_PASS) : null;
+const DB = process.env.DB ;
 mongoose.connect(DB).then(() => {
   console.log("connected to db...");
 });
